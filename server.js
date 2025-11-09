0a5a3300f4302fb34e8e0787e7daea711981a021
my <my.2021.bupt@gmail.com>
2025-11-10 01:21:00 +0800

    update new

--- server.js
+++ server.js
@@ -15,20 +15,27 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
-mongoose.connect(DB, {
-  maxPoolSize: 1, // 减少连接池大小（Serverless 不适合大连接池）
-  serverSelectionTimeoutMS: 5000, // 缩短服务器选择超时（5秒）
-  socketTimeoutMS: 45000, // 延长 socket 超时（避免频繁断开）
-  // keepAlive: true, // 保持连接
-  bufferTimeoutMS: 3000,
-  keepAliveInitialDelay: 300000 // 5分钟发送一次心跳包
-});
-// .then(() => console.log('DB connection successful!'));
+try {
+  console.log('🔗 开始初始化 MongoDB 连接');
+  mongoose.connect(DB, {
+    maxPoolSize: 1, // 减少连接池大小（Serverless 不适合大连接池）
+    serverSelectionTimeoutMS: 5000, // 缩短服务器选择超时（5秒）
+    socketTimeoutMS: 45000, // 延长 socket 超时（避免频繁断开）
+    // keepAlive: true, // 保持连接
+    bufferTimeoutMS: 3000,
+    keepAliveInitialDelay: 300000 // 5分钟发送一次心跳包
+  });
+  console.log('🎉 MongoDB 连接成功');
+  // .then(() => console.log('DB connection successful!'));
+} catch (e) {
+  console.error('❌ MongoDB 连接失败:', e.message);
+  throw err; // 抛出错误，供后续请求捕获
+}
 
 // 监听连接事件，输出状态
-mongoose.connection.on('connected', () => console.log('MongoDB 已连接'));
-mongoose.connection.on('error', err => console.error('连接错误:', err));
-mongoose.connection.on('disconnected', () => console.log('MongoDB 已断开'));
+// mongoose.connection.on('connected', () => console.log('MongoDB 已连接'));
+// mongoose.connection.on('error', err => console.error('连接错误:', err));
+// mongoose.connection.on('disconnected', () => console.log('MongoDB 已断开'));
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
